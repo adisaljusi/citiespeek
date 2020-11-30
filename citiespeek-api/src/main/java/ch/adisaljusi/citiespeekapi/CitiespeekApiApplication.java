f8d16f6f4ffdfa56e1730e0e93b5f6d5116202f8
adisaljusi <adis.aljusi@hotmail.com>
2020-11-30 13:07:50 +0100

    Insert spring boot REST default project

---
+++ citiespeek-api/src/main/java/ch/adisaljusi/citiespeekapi/CitiespeekApiApplication.java
@@ -0,0 +1,13 @@
+package ch.adisaljusi.citiespeekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiespeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiespeekApiApplication.class, args);
+	}
+
+}
---
+++ citiespeek-api/src/test/java/ch/adisaljusi/citiespeekapi/CitiespeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.adisaljusi.citiespeekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiespeekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

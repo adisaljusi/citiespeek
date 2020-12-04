93874d038399577320631fc2d319126b2c862ce6
adisaljusi <adis.aljusi@hotmail.com>
2020-12-04 21:39:42 +0100

    remmove  unused styles in Sidedrawer component

--- citiespeak-fe/src/components/SideDrawer.tsx
+++ citiespeak-fe/src/components/SideDrawer.tsx
@@ -9,7 +9,6 @@ import {
   ListItem,
   ListItemIcon,
   Divider,
-  CssBaseline
 } from '@material-ui/core';
 
 import ExploreIcon from '@material-ui/icons/Explore';
@@ -17,19 +16,13 @@ import ViewListIcon from '@material-ui/icons/ViewList';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: 'flex',
-      height: '100%'
-    },
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-    },
     drawer: {
       width: 60,
       flexShrink: 0,
     },
     drawerPaper: {
       width: 60,
+      overflow: 'hidden'
     }
   }),
 );

27c96879839242b902b8b297d3645756e08c2a70
adisaljusi <adis.aljusi@hotmail.com>
2020-12-05 20:08:21 +0100

    add image to LocationView component and styles

--- citiespeek-fe/src/components/LocationView.tsx
+++ citiespeek-fe/src/components/LocationView.tsx
@@ -1,7 +1,26 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '30%'
+    },
+    image: {
+      objectFit: 'cover',
+      height: '20%',
+      width: '100%'
+    }
+  }),
+);
+
 export const LocationView = () => {
+  const classes = useStyles();
+
   return (
-    <div>sadfjkllsa</div>
+    <div className={classes.root}>
+      <img className={classes.image} src="https://images.unsplash.com/photo-1517771355228-88102bee3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2567&q=80" />
+      
+    </div>
   )
 };

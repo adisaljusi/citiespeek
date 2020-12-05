67a0cbc6598afbbdb239f6f7576375fff594ebd8
adisaljusi <adis.aljusi@hotmail.com>
2020-12-05 19:24:14 +0100

    add marker to map and handle onCLick event

--- citiespeek-fe/src/components/Map.tsx
+++ citiespeek-fe/src/components/Map.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from 'react';
-import ReactMapGL, { InteractiveMap, ViewportProps } from 'react-map-gl';
+import React, { useCallback, useRef, useState } from 'react';
+import { InteractiveMap, Marker, MarkerProps, PointerEvent, ViewportProps } from 'react-map-gl';
 import Geocoder from 'react-map-gl-geocoder';
 import { environment } from '../environment/environment';
+import markerImage from './marker.svg';
 
 export const Map = () => {
   const mapRef = useRef<InteractiveMap>(null);
@@ -10,21 +11,40 @@ export const Map = () => {
     longitude: -122.44847954400961,
     zoom: 12
   } as Partial<ViewportProps>)
+  const [marker, setMarker] = useState<MarkerProps | null>(null);
+
+  const handleViewportChange = useCallback((viewport: Partial<ViewportProps>) => setViewport(viewport), []);
+
+  const handleMapOnClick = (event: PointerEvent) => {
+    event.preventDefault();
+    const [longitude, latitude] = event.lngLat;
+    setMarker({ longitude, latitude });
+  }
 
   return (
-    <ReactMapGL
+    <InteractiveMap
       {...viewport}
       ref={mapRef}
       mapboxApiAccessToken={environment.mapboxConfig.API_KEY}
-      onViewportChange={setViewport}
+      onViewportChange={handleViewportChange}
       width={'100%'}
       height={'100%'}
-    >
+      onClick={handleMapOnClick}>
       <Geocoder
         mapRef={mapRef}
         mapboxApiAccessToken={environment.mapboxConfig.API_KEY}
-        position="top-left"
+        onViewportChange={handleViewportChange}
+        position="top-right"
       />
-    </ReactMapGL>
+      {
+        marker != null && (
+          <Marker {...marker}>
+            <div>
+              <img src={markerImage} />
+            </div>
+          </Marker>
+        )
+      }
+    </InteractiveMap>
   );
 }

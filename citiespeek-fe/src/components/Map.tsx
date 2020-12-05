d2365d08eb2e92384eb8b832f3079c86d6d47704
adisaljusi <adis.aljusi@hotmail.com>
2020-12-05 16:56:48 +0100

    create mapRef

--- citiespeek-fe/src/components/Map.tsx
+++ citiespeek-fe/src/components/Map.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import ReactMapGL, { ViewportProps } from 'react-map-gl';
+import React, { useRef, useState } from 'react';
+import ReactMapGL, { InteractiveMap, ViewportProps } from 'react-map-gl';
 import { environment } from '../environment/environment';
 
 export const Map = () => {
+  const mapRef = useRef<InteractiveMap>(null);
   const [viewport, setViewport] = useState({
     latitude: 37.7510189888697,
     longitude: -122.44847954400961,
@@ -12,10 +13,13 @@ export const Map = () => {
   return (
     <ReactMapGL
       {...viewport}
+      ref={mapRef}
       mapboxApiAccessToken={environment.mapboxConfig.API_KEY}
       onViewportChange={setViewport}
       width={'100%'}
       height={'100%'}
-    />
+    >
+ 
+    </ReactMapGL>
   );
 }

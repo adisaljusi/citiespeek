faf6e853ddaef6cc9d7549436a75f7f650431b84
adisaljusi <adis.aljusi@hotmail.com>
2020-12-05 20:35:05 +0100

    add slide animation to LocationView component

--- citiespeek-fe/src/components/LocationView.tsx
+++ citiespeek-fe/src/components/LocationView.tsx
@@ -1,26 +1,35 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: '30%'
+      width: '30%',
+      zIndex: 121,
+      boxShadow: '2px 2px 4px -1px rgba(0,0,0,0.2), 4px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+      transition: 'width 0.5s ease-in-out'
     },
     image: {
       objectFit: 'cover',
       height: '20%',
       width: '100%'
+    },
+    closed: {
+      width: 0
     }
   }),
 );
+interface Props {
+  readonly imageSrc: string;
+  readonly open: boolean;
+}
 
-export const LocationView = () => {
+export const LocationView: React.FC<Props> = ({ imageSrc, open }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <img className={classes.image} src="https://images.unsplash.com/photo-1517771355228-88102bee3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2567&q=80" />
-      
+    <div className={!open ? classes.closed : classes.root}>
+      <img className={classes.image} src={imageSrc} />
     </div>
   )
 };
--- citiespeek-fe/src/containers/CitiesPeekContainer.tsx
+++ citiespeek-fe/src/containers/CitiesPeekContainer.tsx
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const CitiesPeekContainer = () => {
   const classes = useStyles();
+  const imageSrc = 'https://images.unsplash.com/photo-1517771355228-88102bee3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2567&q=80';
 
   return (
     <>
@@ -44,7 +45,7 @@ export const CitiesPeekContainer = () => {
         </AppBar>
         <SideDrawer />
         <div className={classes.content}>
-          <LocationView />
+          <LocationView imageSrc={imageSrc} open={true}/>
           <Map />
         </div>
       </div>

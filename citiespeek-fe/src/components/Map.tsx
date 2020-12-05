c7b0585daaebc6f7097e869ef0c76d08e2388ca3
adisaljusi <adis.aljusi@hotmail.com>
2020-12-05 16:10:00 +0100

    handle onViewportCHange

--- citiespeek-fe/src/components/Map.tsx
+++ citiespeek-fe/src/components/Map.tsx
@@ -1,12 +1,19 @@
-import mapboxgl from 'mapbox-gl';
-import React, { useEffect, useRef, useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState } from 'react';
+import ReactMapGL, { ViewportProps } from 'react-map-gl';
 import { environment } from '../environment/environment';
+
 export const Map = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7510189888697,
+    longitude: -122.44847954400961,
+    zoom: 12
+  } as Partial<ViewportProps>)
 
   return (
     <ReactMapGL
+      {...viewport}
       mapboxApiAccessToken={environment.mapboxConfig.API_KEY}
+      onViewportChange={setViewport}
       width={'100%'}
       height={'100%'}
     />

caa7eb10db5b859c56e0ed258e5834f11e89128c
adisaljusi <adis.aljusi@hotmail.com>
2020-12-06 20:16:48 +0100

    add Location domain

---
+++ citiespeek-api/src/main/java/ch/adisaljusi/citiespeekapi/Model/Location.java
@@ -0,0 +1,44 @@
+package ch.adisaljusi.citiespeekapi.Model;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Location {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  private UUID id;
+
+  private double  latitude;
+  private double longitude;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+}

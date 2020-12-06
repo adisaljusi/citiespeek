1894fb6484d733c8b18c80ff268e1cbd1a2e143a
adisaljusi <adis.aljusi@hotmail.com>
2020-12-06 21:36:54 +0100

    add lombok to dependencies

--- citiespeek-api/src/main/java/ch/adisaljusi/citiespeekapi/Model/Location.java
+++ citiespeek-api/src/main/java/ch/adisaljusi/citiespeekapi/Model/Location.java
@@ -1,44 +1,21 @@
 package ch.adisaljusi.citiespeekapi.Model;
 
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
+@Data
 public class Location {
   @Id
   @GeneratedValue(generator = "UUID")
   @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
   private UUID id;
 
-  private double  latitude;
+  private double latitude;
   private double longitude;
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public double getLatitude() {
-    return latitude;
-  }
-
-  public void setLatitude(double latitude) {
-    this.latitude = latitude;
-  }
-
-  public double getLongitude() {
-    return longitude;
-  }
-
-  public void setLongitude(double longitude) {
-    this.longitude = longitude;
-  }
 }

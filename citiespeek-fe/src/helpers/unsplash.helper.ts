8618151ecf49b48503cd6aa5c7bc87fc832384f8
adisaljusi <adis.aljusi@hotmail.com>
2020-12-06 17:40:52 +0100

    check for response code 200 in searchImage request

--- citiespeek-fe/src/helpers/unsplash.helper.ts
+++ citiespeek-fe/src/helpers/unsplash.helper.ts
@@ -6,13 +6,21 @@ export const searchImage = async (query: string): Promise<Image | null> => {
 
   return fetch(`${environment.unsplashConfig.URI}/search/photos/?query=${formattedQuery}&client_id=${environment.unsplashConfig.ACCESS_KEY}`, {
     method: 'GET'
-  }).then(res => res.json())
+  }).then(res => {
+    if (res.status === 200) {
+      return res.json()
+    }
+    return null;
+  })
     .then(res => {
-      const { alt_description, urls } = res.results[0];
+      if (res != null) {
+        const { alt_description, urls } = res.results[0];
 
-      return {
-        alt: alt_description,
-        src: urls.regular
-      } as Image;
+        return {
+          alt: alt_description,
+          src: urls.regular
+        } as Image;
+      }
+      return null;
     });
 };
